8c96e6fec62865536c059f6869dce832b84a2055
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-20 12:16:56 +0200

    demo main function

--- src/streamingsystems/Main.java
+++ src/streamingsystems/Main.java
@@ -1,7 +1,15 @@
 package streamingsystems;
 
+import streamingsystems.implemented.MovingItemImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Starting...");
+
+        MovingItem movingItem1 = new MovingItemImpl("Moving Item 1");
+        MovingItem movingItem2 = new MovingItemImpl("Moving Item 2");
+        MovingItem movingItem3 = new MovingItemImpl("Moving Item 3");
+
+        System.out.println("Terminating...");
     }
 }
