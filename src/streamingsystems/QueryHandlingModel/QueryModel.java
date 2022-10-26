e9ab65f30be1b6c10e521b1db19a0810005268af
Aljosha <mail@aljoshavieth.de>
2022-10-26 16:10:20 +0200

    started implementation of EventStore in QueryModel

--- src/streamingsystems/QueryHandlingModel/QueryModel.java
+++ src/streamingsystems/QueryHandlingModel/QueryModel.java
@@ -1,8 +1,10 @@
 package streamingsystems.QueryHandlingModel;
 
+import streamingsystems.CommandsModel.Meta.Event;
 import streamingsystems.MovingItem;
 
 import java.util.HashMap;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class QueryModel {
     private static final QueryModel singletonInstance = new QueryModel();
@@ -17,4 +19,14 @@ public class QueryModel {
 
 
     private HashMap<String, MovingItem> movingItemHashMap = new HashMap<>();
+
+    private HashMap<String, MovingItem> createEventStoreFroEvents(LinkedBlockingQueue<Event> eventQueue) {
+        HashMap<String, MovingItem> map = new HashMap<>();
+        eventQueue.forEach(event -> {
+            switch (event) {
+
+            }
+        });
+
+    }
 }
