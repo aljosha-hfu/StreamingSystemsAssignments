5847e12b39d5c36f141127e3efe9b0ba9a583604
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-26 14:08:15 +0200

    add addEvent and getlastevent methods to eventstore

--- src/streamingsystems/CommandsModel/EventStore.java
+++ src/streamingsystems/CommandsModel/EventStore.java
@@ -7,7 +7,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class EventStore {
     private static final EventStore singletonInstance = new EventStore();
 
-    private LinkedBlockingQueue<MovingItem> movingItemQueue = new LinkedBlockingQueue<>();
+    private LinkedBlockingQueue<Event> eventQueue = new LinkedBlockingQueue<>();
 
 
     private EventStore() {
@@ -16,5 +16,13 @@ public class EventStore {
 
     public static EventStore getInstance() {
         return singletonInstance;
+    }
+
+    public void addEvent(Event event) {
+        this.eventQueue.add(event);
+    }
+
+    public Event getLastEvent() throws InterruptedException {
+        return this.eventQueue.take();
     }
 }
