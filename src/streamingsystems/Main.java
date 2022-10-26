b94ae7ec605401fe4d1e0452eac95519d384ba4c
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-26 15:58:03 +0200

    test commands in main

--- src/streamingsystems/Main.java
+++ src/streamingsystems/Main.java
@@ -1,14 +1,21 @@
 package streamingsystems;
 
+import streamingsystems.CommandsModel.CommandHandler;
 import streamingsystems.implemented.MovingItemDTO;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting...");
 
-        MovingItem movingItem1 = new MovingItemDTO("Moving Item 1");
-        MovingItem movingItem2 = new MovingItemDTO("Moving Item 2");
-        MovingItem movingItem3 = new MovingItemDTO("Moving Item 3");
+        CommandHandler commandHandlerInstance = CommandHandler.getInstance();
+
+        commandHandlerInstance.createItem(new MovingItemDTO("Moving Item 1"));
+        commandHandlerInstance.createItem(new MovingItemDTO("Moving Item 2"));
+        commandHandlerInstance.createItem(new MovingItemDTO("Moving Item 3"));
+        commandHandlerInstance.changeValue("Moving Item 1", 42);
+        commandHandlerInstance.changeValue("Moving Item 2", 69);
+        commandHandlerInstance.changeValue("Moving Item 3", 4711);
+//        commandHandlerInstance.changeValue("Moving Item 4", 4711);
 
         System.out.println("Terminating...");
     }
