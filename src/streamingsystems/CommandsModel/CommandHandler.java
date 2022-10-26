68cd35c43537273f348c72056987ced278dfdddc
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-26 14:45:43 +0200

    rename commandsimpl to commandhandler

--- src/streamingsystems/CommandsModel/CommandHandler.java
+++ src/streamingsystems/CommandsModel/CommandHandler.java
@@ -1,20 +1,40 @@
 package streamingsystems.CommandsModel;
 
 import streamingsystems.CommandsModel.Predefined.Commands;
+import streamingsystems.CommandsModel.commands.*;
+import streamingsystems.MovingItem;
 
-public class CommandHandler {
+public class CommandHandler implements Commands {
+    @Override
+    public void createItem(MovingItem movingItem) {
 
-    private static final CommandHandler singletonInstance = new CommandHandler();
-
-    private CommandHandler() {
-        System.out.println("Instantiated CommandHandler singleton...");
+        Command command = new CreateItemCommand(movingItem);
     }
 
-    public static CommandHandler getInstance() {
-        return singletonInstance;
+    @Override
+    public void deleteItem(String id) {
+        if (id.isBlank()) {
+            throw new IllegalArgumentException("ID must be a valid, non-blank string");
+        }
+
+        Command command = new DeleteItemCommand(id);
     }
 
-    public void handleCommand(Commands commandsToExecute) {
-        // TODO
+    @Override
+    public void moveItem(String id, int[] vector) {
+        if (vector.length != 3) {
+            throw new IllegalArgumentException("Vector must be of length 3 (x, y, z)");
+        }
+
+        Command command = new MoveItemCommand(id, vector);
+    }
+
+    @Override
+    public void changeValue(String id, int newValue) {
+        if (id.isBlank()) {
+            throw new IllegalArgumentException("ID must be a valid, non-blank string");
+        }
+
+        Command command = new ChangeValueCommand(id, newValue);
     }
 }
