e599b76d4b03851380aedde8f6d58e1aee16ee76
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-26 14:12:12 +0200

    rename movingitemplm into dto

--- src/streamingsystems/Main.java
+++ src/streamingsystems/Main.java
@@ -1,14 +1,14 @@
 package streamingsystems;
 
-import streamingsystems.implemented.MovingItemImpl;
+import streamingsystems.implemented.MovingItemDTO;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting...");
 
-        MovingItem movingItem1 = new MovingItemImpl("Moving Item 1");
-        MovingItem movingItem2 = new MovingItemImpl("Moving Item 2");
-        MovingItem movingItem3 = new MovingItemImpl("Moving Item 3");
+        MovingItem movingItem1 = new MovingItemDTO("Moving Item 1");
+        MovingItem movingItem2 = new MovingItemDTO("Moving Item 2");
+        MovingItem movingItem3 = new MovingItemDTO("Moving Item 3");
 
         System.out.println("Terminating...");
     }
--- src/streamingsystems/implemented/MovingItemDTO.java
+++ src/streamingsystems/implemented/MovingItemDTO.java
@@ -1,5 +1,41 @@
 package streamingsystems.implemented;
 
-public class MovingItemDTO {
+import streamingsystems.MovingItem;
 
+public class MovingItemDTO implements MovingItem {
+    private final String name;
+    private final int[] location;
+    private final int numberOfMoves;
+    private final int value;
+
+    public MovingItemDTO(String name, int[] location, int numberOfMoves, int value) {
+        this.name = name;
+        this.location = location;
+        this.numberOfMoves = numberOfMoves;
+        this.value = value;
+    }
+
+    public MovingItemDTO(String name) {
+        this(name, new int[]{0,0,0}, 0, 0);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int[] getLocation() {
+        return location;
+    }
+
+    @Override
+    public int getNumberOfMoves() {
+        return numberOfMoves;
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
 }
