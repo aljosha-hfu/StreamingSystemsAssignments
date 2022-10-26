9f3d8ef6b776f02a3e724f4db9771a52e9e277a7
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-26 14:01:12 +0200

    throw some exceptions for illegal arguments

--- src/streamingsystems/CommandsModel/CommandsImpl.java
+++ src/streamingsystems/CommandsModel/CommandsImpl.java
@@ -10,16 +10,22 @@ public class CommandsImpl implements Commands {
 
     @Override
     public void deleteItem(String id) {
-
+        if (id.isBlank()) {
+            throw new IllegalArgumentException("ID must be a valid, non-blank string");
+        }
     }
 
     @Override
     public void moveItem(String id, int[] vector) {
-
+        if (vector.length != 3) {
+            throw new IllegalArgumentException("Vector must be of length 3 (x, y, z)");
+        }
     }
 
     @Override
     public void changeValue(String id, int newValue) {
-
+        if (id.isBlank()) {
+            throw new IllegalArgumentException("ID must be a valid, non-blank string");
+        }
     }
 }
