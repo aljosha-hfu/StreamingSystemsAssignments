0843308e5152d2130dd535dc21672fb4ab1afb63
Aljosha <mail@aljoshavieth.de>
2022-10-27 15:23:45 +0200

    removed redundant public modifiers for interface

--- src/main/java/streamingsystems/QueryHandlingModel/Predefined/Query.java
+++ src/main/java/streamingsystems/QueryHandlingModel/Predefined/Query.java
@@ -5,7 +5,7 @@ import streamingsystems.implemented.MovingItemDTO;
 import java.util.Enumeration;
 
 public interface Query {
-    public MovingItemDTO getMovingItemByName(String name);
-    public Enumeration<MovingItemDTO> getMovingItems();
-    public Enumeration<MovingItemDTO> getMovingItemsAtPosition(int[] position);
+    MovingItemDTO getMovingItemByName(String name);
+    Enumeration<MovingItemDTO> getMovingItems();
+    Enumeration<MovingItemDTO> getMovingItemsAtPosition(int[] position);
 }
