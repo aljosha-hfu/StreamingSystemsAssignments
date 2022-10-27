e52964936443dd7a5bbe8ba4dfab9d93ddb1d6d7
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-27 11:58:28 +0200

    task 2 part 1 - delete items after certain amount of moves

--- src/main/java/streamingsystems/CommandsModel/commands/MoveItemCommand.java
+++ src/main/java/streamingsystems/CommandsModel/commands/MoveItemCommand.java
@@ -1,8 +1,10 @@
 package streamingsystems.CommandsModel.commands;
 
 
+import streamingsystems.CommandsModel.DomainModel;
 import streamingsystems.CommandsModel.EventStore;
 import streamingsystems.CommandsModel.Meta.Command;
+import streamingsystems.implemented.events.MovingItemDeletedEvent;
 import streamingsystems.implemented.events.MovingItemMovedEvent;
 
 public class MoveItemCommand extends Command {
@@ -16,6 +18,12 @@ public class MoveItemCommand extends Command {
 
     @Override
     public void handle() {
-        EventStore.getInstance().addEvent(new MovingItemMovedEvent(id, vector));
+        if (DomainModel.getInstance().getNumberOfMovesForMovingItemName(id) >= 19) {
+            DomainModel.getInstance().removeMovingItemNameFromModel(id);
+            EventStore.getInstance().addEvent(new MovingItemDeletedEvent(id));
+        } else {
+            EventStore.getInstance().addEvent(new MovingItemMovedEvent(id, vector));
+            DomainModel.getInstance().incrementNumberOfMovesForMovingItemNameByOne(id);
+        }
     }
 }
