3e4564b1e2528707266905359e22159bb6011b80
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-03 12:33:41 +0100

    create class for rabbitmq connection managing

---
+++ src/main/java/streamingsystems/RabbitMQConnectionManager.java
@@ -0,0 +1,50 @@
+package streamingsystems;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import streamingsystems.CommandsModel.EventStore;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitMQConnectionManager {
+    public static final String QUEUE_NAME = "EventStore";
+    private static final RabbitMQConnectionManager singletonInstance = new RabbitMQConnectionManager();
+
+    public static RabbitMQConnectionManager getInstance() {
+        return singletonInstance;
+    }
+
+    private final Connection rabbitMQConnection;
+    private final Channel eventStoreChannel;
+
+    private RabbitMQConnectionManager() {
+        System.out.println("Connecting to RabbitMQ...");
+        ConnectionFactory rabbitMQConnectionFactory = new ConnectionFactory();
+        rabbitMQConnectionFactory.setUsername(ConfigManager.INSTANCE.getRabbitMqUser());
+        rabbitMQConnectionFactory.setPassword(ConfigManager.INSTANCE.getRabbitMqPassword());
+        rabbitMQConnectionFactory.setVirtualHost("/");
+        rabbitMQConnectionFactory.setHost(ConfigManager.INSTANCE.getRabbitMqHost());
+        rabbitMQConnectionFactory.setPort(ConfigManager.INSTANCE.getRabbitMqPort());
+
+        try {
+            rabbitMQConnection = rabbitMQConnectionFactory.newConnection();
+            eventStoreChannel = rabbitMQConnection.createChannel();
+            eventStoreChannel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Connecting to RabbitMQ successful.");
+
+        System.out.println("Instantiated RabbitMQConnectionManager singleton...");
+    }
+
+    public Connection getRabbitMQConnection() {
+        return rabbitMQConnection;
+    }
+
+    public Channel getEventStoreChannel() {
+        return eventStoreChannel;
+    }
+}
