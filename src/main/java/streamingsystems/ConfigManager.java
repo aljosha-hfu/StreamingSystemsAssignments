4bb64af250dd64d6ab87adbccf6f93993fd52f0b
Bastian Hodapp <bastianhodapp@gmail.com>
2022-12-01 11:31:12 +0100

    more cleanup

--- src/main/java/streamingsystems/ConfigManager.java
+++ src/main/java/streamingsystems/ConfigManager.java
@@ -9,12 +9,6 @@ import java.util.Properties;
 public enum ConfigManager {
     INSTANCE;
 
-    private String rabbitMqHost;
-
-    private int rabbitMqPort;
-    private String rabbitMqUser;
-    private String rabbitMqPassword;
-
     private String kafkaTopicName;
 
     private String kafkaClientId;
@@ -25,10 +19,6 @@ public enum ConfigManager {
         try {
             FileInputStream fileInputStream = new FileInputStream("src/main/resources/app.config");
             properties.load(fileInputStream);
-            rabbitMqHost = properties.getProperty("rabbitmq.host");
-            rabbitMqPort = Integer.parseInt(properties.getProperty("rabbitmq.port"));
-            rabbitMqUser = properties.getProperty("rabbitmq.user");
-            rabbitMqPassword = properties.getProperty("rabbitmq.password");
             kafkaTopicName = properties.getProperty("kafka.topicname") + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
             kafkaClientId = properties.getProperty("kafka.clientid");
             kafkaUrl = properties.getProperty("kafka.url");
@@ -39,22 +29,6 @@ public enum ConfigManager {
 
     ConfigManager() {
         loadConfig();
-    }
-
-    public String getRabbitMqHost() {
-        return rabbitMqHost;
-    }
-
-    public int getRabbitMqPort() {
-        return rabbitMqPort;
-    }
-
-    public String getRabbitMqUser() {
-        return rabbitMqUser;
-    }
-
-    public String getRabbitMqPassword() {
-        return rabbitMqPassword;
     }
 
     public String getKafkaTopicName() {
