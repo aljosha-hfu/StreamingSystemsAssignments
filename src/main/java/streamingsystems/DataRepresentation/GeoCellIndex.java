99d0803c5dadfe95028418a6b6a3e9540c13224a
Bastian Hodapp <bastianhodapp@gmail.com>
2022-12-01 12:57:58 +0100

    add more constants

--- src/main/java/streamingsystems/DataRepresentation/GeoCellIndex.java
+++ src/main/java/streamingsystems/DataRepresentation/GeoCellIndex.java
@@ -1,6 +1,10 @@
 package streamingsystems.DataRepresentation;
 
 public class GeoCellIndex {
+    // CONSTANT
+    public static final GeoCellIndex firstCellIndex = new GeoCellIndex(1,1);
+    public static final GeoCellIndex lastCellIndex = new GeoCellIndex(300,300);
+
     public final int xIndex;
     public final int yIndex;
 
