c52feee954d09c3a97f5da529dc9b022d95c046e
Bastian Hodapp <bastianhodapp@gmail.com>
2022-12-01 11:59:25 +0100

    add latlong class

---
+++ src/main/java/streamingsystems/Helpers/LatLong.java
@@ -0,0 +1,12 @@
+package streamingsystems.Helpers;
+
+public class LatLong {
+
+    double lat;
+    double lng;
+
+    public LatLong(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
