290e6a3ef98e0cfd344d90bab25f165c8aaeb1d2
Bastian Hodapp <bastianhodapp@gmail.com>
2022-12-01 12:01:32 +0100

    add taxitrip record class

--- src/main/java/streamingsystems/DataRepresentation/TaxiTrip.java
+++ src/main/java/streamingsystems/DataRepresentation/TaxiTrip.java
@@ -1,5 +1,24 @@
 package streamingsystems.DataRepresentation;
 
-public class TaxiTrip {
+import streamingsystems.Helpers.LatLong;
+import streamingsystems.Helpers.PaymentType;
 
-}
+import java.util.Date;
+
+public record TaxiTrip(
+        String medallion,
+        String hackLicense,
+        Date pickupDatetime,
+        Date dropoffDatetime,
+        int tripTimeInSecs,
+        int tripDistanceInMiles,
+        LatLong pickupLocation,
+        LatLong dropoffLocation,
+        PaymentType paymentType,
+        float fareAmount,
+        float surcharge,
+        float taxDollars,
+        float tipDollars,
+        float tollsAmount,
+        float totalAmount
+) {}
