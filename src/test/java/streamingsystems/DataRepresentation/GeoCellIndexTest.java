9c6370806d641a78cb9e0993c0a8b13797329c45
Bastian Hodapp <bastianhodapp@gmail.com>
2022-12-07 15:15:12 +0100

    add more tests

--- src/test/java/streamingsystems/DataRepresentation/GeoCellIndexTest.java
+++ src/test/java/streamingsystems/DataRepresentation/GeoCellIndexTest.java
@@ -2,17 +2,29 @@ package streamingsystems.DataRepresentation;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GeoCellIndexTest {
 
+    // LATITUDE
     @Test
     void testCellIndexNumberByLatitudeValueIndexOne() {
-        assertEquals(1, GeoCellIndex.getCellIndexNumberByLatitudeValue(GeoCellIndex.firstCellCenterCoords.lat));
+        assertEquals(1, GeoCellIndex.getCellIndexNumberByLatitudeValue(GeoCellIndex.firstCellCenterCoords.lat + 0.3 * GeoCellIndex.latitude500MetersEastDelta));
     }
 
     @Test
+    void testCellIndexNumberByLatitudeValueIndexFive() {
+        assertEquals(5, GeoCellIndex.getCellIndexNumberByLatitudeValue(GeoCellIndex.firstCellCenterCoords.lat + 4.8 * GeoCellIndex.latitude500MetersEastDelta));
+    }
+
+    // LONGITUDE
+    @Test
     void getCellIndexNumberByLongitudeValue() {
-        assertEquals(1, GeoCellIndex.getCellIndexNumberByLongitudeValue(GeoCellIndex.firstCellCenterCoords.lng));
+        assertEquals(1, GeoCellIndex.getCellIndexNumberByLongitudeValue(GeoCellIndex.firstCellCenterCoords.lng + 0.3 * GeoCellIndex.longitude500MetersSouthDelta));
+    }
+
+    @Test
+    void getCellIndexNumberByLongitudeValueIndexFive() {
+        assertEquals(5, GeoCellIndex.getCellIndexNumberByLongitudeValue(GeoCellIndex.firstCellCenterCoords.lng + 4.8 * GeoCellIndex.longitude500MetersSouthDelta));
     }
 }
