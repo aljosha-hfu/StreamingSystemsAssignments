03d9eb54312af184c379a988283cfcc9878ca7f5
Aljosha <mail@aljoshavieth.de>
2022-12-08 14:25:11 +0100

    added third PaymentType "UNKNOWN"

--- src/main/java/streamingsystems/DataRepresentation/PaymentType.java
+++ src/main/java/streamingsystems/DataRepresentation/PaymentType.java
@@ -2,12 +2,11 @@ package streamingsystems.DataRepresentation;
 
 
 public enum PaymentType {
-
     CRD("CREDIT_CARD"),
-    CSH("CASH");
+    CSH("CASH"),
+    UNK("UNKNOWN");
 
     public final String name;
-
     PaymentType(String name) {
         this.name = name;
     }
