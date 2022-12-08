610a410923482f8ca4febcddfdd7cd1b2dbecb54
Aljosha <mail@aljoshavieth.de>
2022-12-08 14:37:30 +0100

    made LatLong a record

--- src/main/java/streamingsystems/DataRepresentation/LatLong.java
+++ src/main/java/streamingsystems/DataRepresentation/LatLong.java
@@ -1,12 +1,3 @@
 package streamingsystems.DataRepresentation;
 
-public class LatLong {
-
-    double lat;
-    double lng;
-
-    public LatLong(double lat, double lng) {
-        this.lat = lat;
-        this.lng = lng;
-    }
-}
+record LatLong(double lat, double lng) {}
