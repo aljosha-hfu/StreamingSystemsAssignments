843d5e3bc81cd2009cb4d3db4a647cbecb6fc15c
Bastian Hodapp <bastianhodapp@gmail.com>
2022-12-08 15:33:48 +0100

    reformat tests

--- src/test/java/streamingsystems/DataRepresentation/GeoCellIndexTest.java
+++ src/test/java/streamingsystems/DataRepresentation/GeoCellIndexTest.java
@@ -9,42 +9,50 @@ class GeoCellIndexTest {
     // LATITUDE
     @Test
     void testCellIndexNumberByLatitudeValueIndex1() {
-        assertEquals(1, GeoCellIndex.getCellIndexNumberByLatitudeValue(GeoCellIndex.firstCellCenterCoords.lat - 0.3 * GeoCellIndex.latitude500MetersEastDelta));
+        assertEquals(1, GeoCellIndex.getCellIndexNumberByLatitudeValue(
+                GeoCellIndex.firstCellCenterCoords.lat - 0.3 * GeoCellIndex.latitude500MetersEastDelta));
     }
 
     @Test
     void testCellIndexNumberByLatitudeValueIndex5() {
-        assertEquals(5, GeoCellIndex.getCellIndexNumberByLatitudeValue(GeoCellIndex.firstCellCenterCoords.lat - 5.4 * GeoCellIndex.latitude500MetersEastDelta));
+        assertEquals(5, GeoCellIndex.getCellIndexNumberByLatitudeValue(
+                GeoCellIndex.firstCellCenterCoords.lat - 5.4 * GeoCellIndex.latitude500MetersEastDelta));
     }
 
     @Test
     void testCellIndexNumberByLatitudeValueIndex255() {
-        assertEquals(255, GeoCellIndex.getCellIndexNumberByLatitudeValue(GeoCellIndex.firstCellCenterCoords.lat - 255.8 * GeoCellIndex.latitude500MetersEastDelta));
+        assertEquals(255, GeoCellIndex.getCellIndexNumberByLatitudeValue(
+                GeoCellIndex.firstCellCenterCoords.lat - 255.8 * GeoCellIndex.latitude500MetersEastDelta));
     }
 
     @Test
     void testCellIndexNumberByLatitudeValueIndex300() {
-        assertEquals(300, GeoCellIndex.getCellIndexNumberByLatitudeValue(GeoCellIndex.firstCellCenterCoords.lat - 300.8 * GeoCellIndex.latitude500MetersEastDelta));
+        assertEquals(300, GeoCellIndex.getCellIndexNumberByLatitudeValue(
+                GeoCellIndex.firstCellCenterCoords.lat - 300.8 * GeoCellIndex.latitude500MetersEastDelta));
     }
 
     // LONGITUDE
     @Test
     void getCellIndexNumberByLongitudeValueIndex1() {
-        assertEquals(1, GeoCellIndex.getCellIndexNumberByLongitudeValue(GeoCellIndex.firstCellCenterCoords.lng + 0.3 * GeoCellIndex.longitude500MetersSouthDelta));
+        assertEquals(1, GeoCellIndex.getCellIndexNumberByLongitudeValue(
+                GeoCellIndex.firstCellCenterCoords.lng + 0.3 * GeoCellIndex.longitude500MetersSouthDelta));
     }
 
     @Test
     void getCellIndexNumberByLongitudeValueIndex8() {
-        assertEquals(8, GeoCellIndex.getCellIndexNumberByLongitudeValue(GeoCellIndex.firstCellCenterCoords.lng + 8.2 * GeoCellIndex.longitude500MetersSouthDelta));
+        assertEquals(8, GeoCellIndex.getCellIndexNumberByLongitudeValue(
+                GeoCellIndex.firstCellCenterCoords.lng + 8.2 * GeoCellIndex.longitude500MetersSouthDelta));
     }
 
     @Test
     void getCellIndexNumberByLongitudeValueIndex197() {
-        assertEquals(197, GeoCellIndex.getCellIndexNumberByLongitudeValue(GeoCellIndex.firstCellCenterCoords.lng + 197.2 * GeoCellIndex.longitude500MetersSouthDelta));
+        assertEquals(197, GeoCellIndex.getCellIndexNumberByLongitudeValue(
+                GeoCellIndex.firstCellCenterCoords.lng + 197.2 * GeoCellIndex.longitude500MetersSouthDelta));
     }
 
     @Test
     void getCellIndexNumberByLongitudeValueIndex300() {
-        assertEquals(300, GeoCellIndex.getCellIndexNumberByLongitudeValue(GeoCellIndex.firstCellCenterCoords.lng + 300.2 * GeoCellIndex.longitude500MetersSouthDelta));
+        assertEquals(300, GeoCellIndex.getCellIndexNumberByLongitudeValue(
+                GeoCellIndex.firstCellCenterCoords.lng + 300.2 * GeoCellIndex.longitude500MetersSouthDelta));
     }
 }
