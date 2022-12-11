b827b7efc0d87327f671bddcc4c057cdc9114e85
Bastian Hodapp <bastianhodapp@gmail.com>
2022-12-11 21:45:06 +0100

    add more example code

--- src/main/java/streamingsystems/Main.java
+++ src/main/java/streamingsystems/Main.java
@@ -5,7 +5,10 @@ import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
+import org.joda.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,6 +27,19 @@ public class Main {
         Pipeline pipeline = Pipeline.create(options);
         // Create the PCollection
         PCollection<String> pc = pipeline.apply(Create.of(cities)).setCoder(StringUtf8Coder.of());
+
+        PCollection<String> pcWithTimestamp = // Note: this examples
+                pc.apply(ParDo.of(new DoFn<String, String>() { // uses an anonymous
+                    @DoFn.ProcessElement // class
+                    public void processElement(@Element String input, OutputReceiver<String> output) {
+                        // Get current time with type org.joda.time.Instant
+                        Instant i = Instant.now();
+                        // The input element gets a new timestamp with the current time
+                        // and is passed to the output PCollection.
+                        output.outputWithTimestamp(input, i);
+                    }
+                }));
+
         // Start the execution
         pipeline.run().waitUntilFinish();
 
