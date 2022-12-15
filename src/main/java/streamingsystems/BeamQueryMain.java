d5a97eefad50ca3a8e8a95b0aed55db17f2840df
Bastian Hodapp <bastianhodapp@gmail.com>
2022-12-15 12:39:37 +0100

    continue query stuff

--- src/main/java/streamingsystems/BeamQueryMain.java
+++ src/main/java/streamingsystems/BeamQueryMain.java
@@ -7,6 +7,7 @@ import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -41,21 +42,42 @@ public class BeamQueryMain {
                                                                                                                    .parse("1990-01-01")
                                                                                                                    .toDateTimeAtCurrentTime()
                                                                                                                    .toInstant()));
+        // pardo into a new PCollection as arrays with keys
+        PCollection<KV<Integer, String[]>> parsedRecords = kafkaRecords.apply(
+                ParDo.of(new DoFn<KafkaRecord<Integer, String>, KV<Integer, String[]>>() {
+                    @ProcessElement
+                    public void processElement(@Element KafkaRecord<Integer, String> inputRecord,
+                                               OutputReceiver<KV<Integer, String[]>> outputRecord) {
+                        String[] splitSensorValues = inputRecord.getKV().getValue().split(",");
+
+                        //Remove negative values
+                        for (int i = 0; i < splitSensorValues.length; i++) {
+                            if (splitSensorValues[i].startsWith("-")) {
+                                splitSensorValues[i] = splitSensorValues[i].substring(1);
+                            }
+                        }
+
+                        outputRecord.output(KV.of(inputRecord.getKV().getKey(), splitSensorValues));
+                    }
+                }));
+
+        // Group messages by key
+        PCollection<KV<Integer, Iterable<String[]>>> groupedSpeedRecords = parsedRecords.apply(
+                org.apache.beam.sdk.transforms.GroupByKey.create());
 
         // Print the collection
-        kafkaRecords.apply(ParDo.of(new DoFn<KafkaRecord<Integer, String>, Void>() {
+        groupedSpeedRecords.apply(ParDo.of(new DoFn<KV<Integer, Iterable<String[]>>, Void>() {
             @ProcessElement
-            public void processElement(ProcessContext c) {
-                KafkaRecord<Integer, String> kafkaRecord = c.element();
-                logger.info("Key: " + kafkaRecord.getKV().getKey() + ", Value: " + kafkaRecord.getKV().getValue() +
-                                    ", Timestamp: " + kafkaRecord.getTimestamp());
+            public void processElement(@Element KV<Integer, Iterable<String[]>> record) {
+                System.out.println(record.getKey());
+                for (String[] sensorValues : record.getValue()) {
+                    System.out.println("  " + sensorValues[0] + " " + sensorValues[1] + " " + sensorValues[2]);
+                }
             }
         }));
 
         // Start the execution
         pipeline.run().waitUntilFinish();
-
-        // print the PCollection
 
         logger.info("Terminating...");
     }
