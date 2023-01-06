d88e949c05221534bf7f5dcb5a75b74e9f0af87a
Bastian Hodapp <bastianhodapp@gmail.com>
2023-01-06 22:20:50 +0100

    add dummy code to dataparser

--- src/main/java/streamingsystems/DataParser.java
+++ src/main/java/streamingsystems/DataParser.java
@@ -1,7 +1,19 @@
 package streamingsystems;
 
+import com.espertech.esper.common.client.EPCompiled;
+import com.espertech.esper.common.client.configuration.Configuration;
+import com.espertech.esper.compiler.client.CompilerArguments;
+import com.espertech.esper.compiler.client.EPCompileException;
+import com.espertech.esper.compiler.client.EPCompiler;
+import com.espertech.esper.compiler.client.EPCompilerProvider;
+import com.espertech.esper.runtime.client.EPDeployException;
+import com.espertech.esper.runtime.client.EPDeployment;
+import com.espertech.esper.runtime.client.EPRuntime;
+import com.espertech.esper.runtime.client.EPRuntimeProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import streamingsystems.events.AverageSpeedEvent;
+import streamingsystems.events.TrafficJamEvent;
 
 import java.io.Serializable;
 
@@ -17,11 +29,24 @@ public class DataParser implements Serializable {
     /**
      * The main method for this class.
      */
-    public void parse() {
+    public void parse() throws EPCompileException, EPDeployException {
         Logger logger = LoggerFactory.getLogger(DataParser.class.getName());
         logger.info("Starting...");
 
+        String q1 = "@name('getSensorsEvents‘) select id, speed from SensorEvent;\n";
+        String q2 = "create context EventsById partition by id from SensorEvent;\n";
 
+        Configuration configuration = new Configuration();
+        configuration.getCommon().addEventType(AverageSpeedEvent.class);
+        configuration.getCommon().addEventType(TrafficJamEvent.class);
+
+        EPCompiler compiler = EPCompilerProvider.getCompiler();
+        CompilerArguments args = new CompilerArguments(configuration);
+        EPCompiled epCompiled = compiler.compile(q1 + q2, args);
+
+        EPRuntime runtime = EPRuntimeProvider.getDefaultRuntime(configuration);
+        runtime.initialize();
+        EPDeployment deployment = runtime.getDeploymentService().deploy(epCompiled);
 
         logger.info("Terminating...");
     }
--- src/main/java/streamingsystems/Main.java
+++ src/main/java/streamingsystems/Main.java
@@ -1,5 +1,8 @@
 package streamingsystems;
 
+import com.espertech.esper.compiler.client.EPCompileException;
+import com.espertech.esper.runtime.client.EPDeployException;
+
 /**
  * A class for starting Kafka test data generation and parsing thereof.
  */
@@ -8,7 +11,7 @@ public class Main {
     /**
      * @param args The command line arguments.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws EPDeployException, EPCompileException {
         new TestDataGeneratorThread().start();
         new DataParser().parse();
     }
