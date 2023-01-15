f1293a3bd67a8260d7d394d0e8315cc4789b29dc
Bastian Hodapp <bastianhodapp@gmail.com>
2023-01-15 22:00:40 +0100

    add average speed event listener class

---
+++ src/main/java/streamingsystems/eventlisteners/AverageSpeedEventListener.java
@@ -0,0 +1,26 @@
+package streamingsystems.eventlisteners;
+
+import com.espertech.esper.common.client.EventBean;
+import com.espertech.esper.runtime.client.EPRuntime;
+import com.espertech.esper.runtime.client.EPStatement;
+import com.espertech.esper.runtime.client.UpdateListener;
+
+import java.util.Arrays;
+
+/**
+ * Event listener for avg speed events
+ */
+public class AverageSpeedEventListener implements UpdateListener {
+    @Override public void update(
+            EventBean[] newEvents, EventBean[] oldEvents, EPStatement statement, EPRuntime runtime
+    ) {
+        // newEvents enthäht die Ereignisinstanzen,
+        // die die Anfrage erfüllen
+
+        //System.out.println(Arrays.toString(newEvents));
+        Arrays.stream(newEvents).forEach(x -> System.out.println("AVERAGE SPEED: for sensor "
+                                                                 + x.get("sensorId")
+                                                                 + ": "
+                                                                 + x.get("averageSpeed")));
+    }
+}
