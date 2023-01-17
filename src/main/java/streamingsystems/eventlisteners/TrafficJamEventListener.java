509a8cfb301a751880f743cd66518829b3ec25bb
Bastian Hodapp <bastianhodapp@gmail.com>
2023-01-17 13:42:33 +0100

    add trafficjameventlisteener for later

---
+++ src/main/java/streamingsystems/eventlisteners/TrafficJamEventListener.java
@@ -0,0 +1,14 @@
+package streamingsystems.eventlisteners;
+
+import com.espertech.esper.common.client.EventBean;
+import com.espertech.esper.runtime.client.EPRuntime;
+import com.espertech.esper.runtime.client.EPStatement;
+import com.espertech.esper.runtime.client.UpdateListener;
+
+public class TrafficJamEventListener implements UpdateListener {
+    @Override public void update(
+            EventBean[] newEvents, EventBean[] oldEvents, EPStatement statement, EPRuntime runtime
+    ) {
+        System.out.println("TRAFFIC JAM OCCURRED: for sensor " + newEvents[0].get("sensorId"));
+    }
+}
