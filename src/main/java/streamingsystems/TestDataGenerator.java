5291e8f57ed08b310ae6e39ea8a4de8d20cc79c1
Aljosha Vieth <mail@aljoshavieth.de>
2023-01-19 12:57:55 +0100

    modified TestDataGenerator. Currently crashing.

--- src/main/java/streamingsystems/TestDataGenerator.java
+++ src/main/java/streamingsystems/TestDataGenerator.java
@@ -5,7 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import streamingsystems.events.SensorEvent;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.StringJoiner;
 
 /**
  * This class generates test data of virtual sensors and sends them to esper.
@@ -43,15 +46,55 @@ public class TestDataGenerator {
             float minSpeed, float maxSpeed, int amountOfSensors, int amountOfSpeedValues, int m1, int m2
     ) throws InterruptedException {
         // Negative speed values should be possible
-        // Next step: generate random speed values with a time skip between m1 and m2
+        // Next step: generate random speed values with a time skip between m1 and m
 
         // Traffic jam counter
         int trafficJamCounter = 1;
         int trafficJamFrequency = 50;
 
+        int minTrafficJamSpeed = 0;
+        int maxTrafficJamSpeed = 6;
+
         int randomSensorId;
         int randomAmountOfGeneratedSpeedValues;
 
+
+
+        while (true) {
+            randomSensorId = (int)(randomGenerator.nextDouble() * amountOfSensors);
+            randomAmountOfGeneratedSpeedValues = (int)(randomGenerator.nextDouble() * amountOfSpeedValues);
+
+            StringJoiner speedValueStringBuilder = new StringJoiner(",");
+
+            // Generate random speed values
+            for (int i = 0; i < randomAmountOfGeneratedSpeedValues; i++) {
+                float randomSpeedValue = randomGenerator.nextFloat() * (maxSpeed - minSpeed) + minSpeed;
+                speedValueStringBuilder.add(String.valueOf(randomSpeedValue));
+            }
+
+            // Generate additional speed events that simulate traffic jam every 50 times
+            if (trafficJamCounter % trafficJamFrequency == 0) {
+                System.out.println("Simulating TRAFFIC JAM for sensor ID: " + randomSensorId);
+                // Send the event 100 times
+                for (int j = 0; j < 100; j++) {
+                    float
+                            randomTrafficJamSpeedValue =
+                            randomGenerator.nextFloat() * (maxTrafficJamSpeed- minTrafficJamSpeed) + minTrafficJamSpeed;
+                    speedValueStringBuilder.add(String.valueOf(randomTrafficJamSpeedValue));
+                }
+            }
+
+            sendSensorEvents(generateSensorEventList(randomSensorId,speedValueStringBuilder.toString()));
+            //logger.info("Sent record: " + recordToSend);
+
+            long timeToSleep = (long)(randomGenerator.nextDouble() * (m2 - m1) + m1);
+            Thread.sleep(timeToSleep);
+            // Increase traffic jam counter
+            trafficJamCounter++;
+        }
+
+
+        /*
         while (true) {
             // Random values
             randomSensorId = (int)(randomGenerator.nextDouble() * amountOfSensors);
@@ -80,8 +123,25 @@ public class TestDataGenerator {
             // Increase traffic jam counter
             trafficJamCounter++;
         }
+
+         */
     }
 
+    // TODO: fix bug. The speedvalue is sometimes not an double. Check split and joiner.
+    private ArrayList<SensorEvent> generateSensorEventList(int sensorId, String speedValues){
+        ArrayList<SensorEvent> sensorEvents = new ArrayList<>();
+        Arrays.stream(speedValues.split(",")).forEach(speedValue -> sensorEvents.add(new SensorEvent(sensorId, Double.parseDouble(speedValue))));
+        return sensorEvents;
+    }
+
+    private void sendSensorEvents(ArrayList<SensorEvent> sensorEvents){
+        sensorEvents.forEach(sensorEvent -> {
+            EsperClient.getINSTANCE().getSensorEventSender().sendEvent(sensorEvent);
+            logger.info("Sent SensorEvent: sensorId= " + sensorEvent.getSensorId() + " speed: " + sensorEvent.getSpeed());
+        });
+    }
+
+    /*
     private void sendRandomDataMessage(float thisSensorMinSpeed, float thisSensorMaxSpeed, int randomSensorId) {
         float
                 randomSpeedValue =
@@ -90,4 +150,6 @@ public class TestDataGenerator {
         SensorEvent sensorEvent = new SensorEvent(randomSensorId, randomSpeedValue);
         EsperClient.getINSTANCE().getSensorEventSender().sendEvent(sensorEvent);
     }
+
+     */
 }
