dda9551ed5d9cda443451cf60efc0d17fa8f234b
Aljosha <mail@aljoshavieth.de>
2023-02-10 23:12:32 +0100

    code cleanup

--- src/main/java/streamingsystems01/commandsmodel/CommandHandler.java
+++ src/main/java/streamingsystems01/commandsmodel/CommandHandler.java
@@ -81,7 +81,6 @@ public class CommandHandler implements Commands {
      */
     @Override
     public void moveItem(String id, int[] vector) {
-        // TODO: Is this alright?
         if (vector.length != 3) {
             throw new IllegalArgumentException(
                     "Vector must be of length 3 (x, y, z)");
